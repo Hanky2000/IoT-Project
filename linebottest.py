bf067e4b3528924176348c9d316aa3abf1ea4d6b
Hanky2000 <86181854+Hanky2000@users.noreply.github.com>
2022-01-09 23:37:36 +0800

    Update linebottest.py

--- linebottest.py
+++ linebottest.py
@@ -5,6 +5,7 @@ import time
 import RPi.GPIO as gpio
 from picamera import PiCamera
 import cv2
+import numpy as np
 # Use BCM GPIO references
 # instead of physical pin numbers
 gpio.setmode(gpio.BOARD)
@@ -124,37 +125,39 @@ def handle_message(event):
     else:                
         line_bot_api.reply_message(reply_token, TextSendMessage(text = "物件未登錄"))
 def lookfor(string):
-        
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())    
     # open webcam video stream
     cap = cv2.VideoCapture(0)    
-    sequences = [0, 1, 2, 3, 4, 5]
-    for i in sequences:
+    while(True):
         # Capture frame-by-frame
         ret, frame = cap.read()
 
         # resizing for faster detection
         frame = cv2.resize(frame, (640, 480))
         # using a greyscale picture, also for faster detection
-        #gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
         # detect people in the image
         # returns the bounding boxes for the detected objects
-        #boxes, weights = hog.detectMultiScale(frame, winStride=(8,8) )
+        boxes, weights = hog.detectMultiScale(frame, winStride=(8,8) )
 
-        #boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
-        '''
+        boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+        
         for (xA, yA, xB, yB) in boxes:
             # display the detected boxes in the colour picture
             cv2.rectangle(frame, (xA, yA), (xB, yB),
                               (0, 255, 0), 2)
-        '''
+        
         # Display the resulting frame
         cv2.imshow('frame',frame)
-        forward(60, 0.01)
-        time.sleep(5)
+        
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-        
+    sequences = [0, 1, 2, 3, 4, 5]
+    for i in sequences:
+        forward(60, 0.01)
+        time.sleep(5)        
         
             
     # When everything done, release the capture
@@ -169,3 +172,4 @@ if __name__ == "__main__":
     app.run(host='0.0.0.0', port=port)
 
 gpio.cleanup()
+
